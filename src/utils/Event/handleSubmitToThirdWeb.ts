965b784432a9d236086ab0047dee781b3f0b3228
joscacgom <joscacgom@alum.us.es>
2023-05-20 16:34:33 +0200

    chore: claim conditions

--- src/utils/Event/handleSubmitToThirdWeb.ts
+++ src/utils/Event/handleSubmitToThirdWeb.ts
@@ -1,20 +1,25 @@
 import { TicketTableSupabase } from '@/models/Tickets/types'
 import { useContract, useContractMetadata, useSDK } from '@thirdweb-dev/react'
+import { getUserCookie } from '@/utils/Login/userCookie'
 
 export const handleSubmitToThirdWeb = async (ticket: TicketTableSupabase) => {
   const sdk = useSDK()
-
+  const organizerAddress = JSON.parse(getUserCookie('userData')).address
   try {
     const nftContract: any = await sdk?.deployer.deployNFTDrop({
       name: ticket.name,
-      primary_sale_recipient: '{{wallet_address}}', // wallet address of the event organizer connected to the app
+      primary_sale_recipient: organizerAddress,
       image: ticket.image,
-      description: ticket.description
-      // fees
-
+      description: ticket.description,
+      fee_recipient: organizerAddress,
+      seller_fee_basis_points: ticket.organizer_royalty,
+      platform_fee_recipient: ticket.platform_address,
+      platform_fee_basis_points: ticket.platform_royalty
     })
     const { contract } = useContract(nftContract)
     const { data: contractMetadata } = useContractMetadata(contract)
+    await sdk?.getContract(nftContract, 'nft-drop')
+
     const nftImageURL = contractMetadata?.image
 
     return { nftContract, nftImageURL }
