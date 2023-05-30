c8afa8c523be263c825b6766b4d841baab3d7bf5
jvegax <toledo.utrera98@gmail.com>
2023-05-30 19:16:14 +0200

    chore: random edit2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eventverse
+
+edit
--- src/utils/Login/handleWeb3AuthInit.ts
+++ src/utils/Login/handleWeb3AuthInit.ts
@@ -11,7 +11,7 @@ import { TorusWalletAdapter } from '@web3auth/torus-evm-adapter'
 import { getUserCookie, setCookie, removeCookie } from '@/utils/Login/userCookie'
 import EthereumRpc from '@/utils/Login/web3RPC'
 
-console.log('hello')
+console.log('hello2')
 const clientId = process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID as string
 const walletConnectClientId = process.env.NEXT_PUBLIC_WALLET_CONNECT_CLIENT_ID as string
 
