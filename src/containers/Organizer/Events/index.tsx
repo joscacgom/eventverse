fb51a47c848caf7fea696503898c439cbd833b9a
Jorge Vega <49287932+jvegax@users.noreply.github.com>
2023-06-04 21:14:46 +0200

    refactor: events handler refactor

--- src/containers/Organizer/Events/index.tsx
+++ src/containers/Organizer/Events/index.tsx
@@ -6,22 +6,13 @@ import { getUserCookie } from '@/utils/Login/userCookie'
 const OrganizerEvents = () => {
   const userCookie = getUserCookie('userData')
   const { events, error, isLoading } = useEventsByUser({ userEmail: userCookie?.email })
+  
   const renderOrganizerEvents = () => {
-    if (events === undefined || error) {
-      return <div>Hubo un error al cargar los eventos</div>
-    }
-
-    if (isLoading) {
-      return <div>Cargando eventos...</div>
-    }
-
-    if (events.length === 0) {
-      return <div>Aún no tienes eventos creados</div>
-    }
-
-    return events.map(
-      (event) => <EventCard key={event.id} event={event} />
-    )
+    if (events === undefined || error) return <div>Hubo un error al cargar los eventos</div>
+    if (isLoading) return <div>Cargando eventos...</div>
+    if (events.length === 0) return <div>Aún no tienes eventos creados</div>
+    
+    return events.map(event => <EventCard key={event.id} event={event} />)
   }
 
   return (
