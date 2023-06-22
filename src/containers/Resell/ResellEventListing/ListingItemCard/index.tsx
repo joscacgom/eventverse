09a38df0379b4e52b10812f901c331a4e4531885
joscacgom <joscacgom@alum.us.es>
2023-06-22 19:29:10 +0200

    chore: style changes

--- src/containers/Resell/ResellEventListing/ListingItemCard/index.tsx
+++ src/containers/Resell/ResellEventListing/ListingItemCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { CardContent, CardImage, CardWrapper, Price, Title, BuyButton } from './styles'
 import { TicketListingItem } from '@/models/Resell/types'
 import { ToastContainer, Zoom, toast } from 'react-toastify'
@@ -13,12 +13,31 @@ const EventCard: FC<Props> = ({ listingItem }) => {
   const { contract } = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_ADDR, 'marketplace')
   const networkMismatch = useNetworkMismatch()
   const userAddress = useAddress()
+  const [eurBalance, setEurBalance] = useState<number>(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/crypto')
+        if (!response.ok) {
+          throw new Error('Failed to fetch data from the Crypto conversor API')
+        }
+        const data = await response.json()
+        const eurBalance = (Number(maticPrice) * data).toFixed(2)
+        setEurBalance(Number(eurBalance))
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchData()
+  }, [])
 
   const onClickBuyTicket = async () => {
     if (!contract) return
     try {
       await contract.buyoutListing(listingItem.id, 1)
-      toast.success('Ticket comprado con éxito 🎉')
+      toast.success('Ticket comprado con éxito 🎉, lo podrás ver únicamente en tu wallet de metamask')
     } catch (error) {
       toast.error('Error al comprar ticket, comprueba que tienes sufiente MATIC en tu wallet 🙃')
     }
@@ -31,7 +50,7 @@ const EventCard: FC<Props> = ({ listingItem }) => {
       <CardImage src={listingItem.asset.image} alt="ticket image" />
       <CardContent>
         <Title>{listingItem.asset.name}</Title>
-        <Price>{maticPrice} MATIC</Price>
+        <Price>{eurBalance}€ <small> {Number(maticPrice).toFixed(4)} ~ MATIC </small></Price>
         {
           (networkMismatch || !userAddress)
             ? <ConnectWallet data-testid="connect-wallet"/>
